674eb1079d4149cce29991aef549bc9c0d1f904b
천지은 <jieun@MacBook-Pro-5.local>
2022-02-17 19:05:00 +0900

    Update: request complete

--- students/jungchan/users/models.py
+++ students/jungchan/users/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class UserInformation(models.Model):
-    name        = models.CharField(max_length=20)
-    email       = models.CharField(max_length=30)
-    password    = models.CharField(max_length=20)
-    phonenumber = models.CharField(max_length=20)
+    name         = models.CharField(max_length=20)
+    email        = models.CharField(max_length=30, unique=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=50)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
-        
