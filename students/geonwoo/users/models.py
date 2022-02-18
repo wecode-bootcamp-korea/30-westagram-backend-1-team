a6def8e1f9f15e7212731e06f0b728e44f78f84f
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-02-18 11:12:43 +0900

    modified: modeling4

--- students/geonwoo/users/models.py
+++ students/geonwoo/users/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name         = models.CharField(max_length=50, unique=True)
+    name         = models.CharField(max_length=50)
     email        = models.CharField(max_length=50,unique=True)
-    password     = models.CharField(max_length=150,unique=True)
+    password     = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=50,unique=True)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
