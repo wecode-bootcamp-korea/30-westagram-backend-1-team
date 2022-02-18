e8c51e3d51cf1d0a755bf6688bdfb9c8105c136f
jiwon <lizzy@192.168.nate.com>
2022-02-18 15:05:40 +0900

    Add user app

--- students/jiwon/users/models.py
+++ students/jiwon/users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=10)
-    email = models.CharField(max_length=30)
-    password = models.CharField(max_length=20)
+    name         = models.CharField(max_length=50)
+    email        = models.CharField(max_length=100)
+    password     = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
+    created_at   = models.DataTimeField(auto_now_add=True)
+    updated_at   = models.DataTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'users' 
+        db_table = 'users'
 
