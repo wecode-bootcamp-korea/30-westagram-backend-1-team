c25676aef10c024aa346ce7a7a2e2e274f78df68
천지은 <jieun@MacBook-Pro-5.local>
2022-02-21 17:04:12 +0900

    Create: Login developed

--- students/jungchan/users/views.py
+++ students/jungchan/users/views.py
@@ -34,3 +34,21 @@ class SignupView(View):
         except KeyError:
             return JsonResponse({'message':'KeyError'}, status=400) 
             
+class LoginView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            email = data['email']
+            password = data['password']
+
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'message':'INVALID_EMAIL'}, status=401)
+
+            if not User.objects.filter(password=password).exists():
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status=401)
+
+            if User.objects.filter(email=email, password=password).exists():
+                return JsonResponse({'message':'SUCCESS'}, status=200)
+            
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
