240d1ac8e48f8dacd36e999008950be028448103
천지은 <jieun@MacBook-Pro-5.local>
2022-02-21 16:07:08 +0900

    Update: signup updated

--- students/jungchan/users/views.py
+++ students/jungchan/users/views.py
@@ -1,30 +1,34 @@
-from django.shortcuts import render
-
 import json, re
 
 from django.http import JsonResponse
 from django.views import View
 
-from users.models import UserInformation
+from users.models import User
+from users.validation import valid_email, valid_password
 
 class SignupView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', data['email']):
+            data         = json.loads(request.body)
+            name         = data['name']
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
+
+            if not valid_email(email):
                 return JsonResponse({'message':'Invalid_email'}, status=400)
             
-            if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$', data['password']):
+            if not valid_password(password):
                 return JsonResponse({'message':'Invalid_password'}, status=400)  
             
-            if UserInformation.objects.filter(email=data['email']).exists(): 
+            if User.objects.filter(email=email).exists(): 
                     return JsonResponse({'message':'Overlapped_email'}, status=400)
             
-            signup = UserInformation.objects.create(
-                name         = data['name'],
-                email        = data['email'],
-                password     = data['password'],
-                phone_number = data['phone_number']
+            signup = User.objects.create(
+                name         = name,
+                email        = email,
+                password     = password,
+                phone_number = phone_number
             )
             return JsonResponse({'message':'SUCCESS'}, status=201)
         except KeyError:
