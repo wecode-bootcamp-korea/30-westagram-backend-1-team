f0b7c72106c90e71686ad224330f8a54bd731fb0
lizzy <gee902722@gmail.com>
2022-02-21 16:44:35 +0900

    Add signup

--- students/jiwon/users/views.py
+++ students/jiwon/users/views.py
@@ -11,27 +11,28 @@ class UserView(View):
         try:
             data = json.loads(request.body)
 
+            self.validate_email(data["email"])
+            self.validate_password(data["password"])
+
             user = User.objects.create(
                 name         = data['name'],
                 email        = data['email'],
                 password     = data['password'],
-                phone_number = data['phone_number'],
-                etc          = data['etc']
+                phone_number = data['phone_number']
             )
             return JsonResponse({"message":"SUCCESS"}, status=201)
 
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
 
-    def validate_email(email):
+    def validate_email(self, email):
         REGEX_EMAIL = '([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
-        if not re.fullmatch(REGEX_EMAIL, email):
-            return JsonResponse(["이메일 형식을 확인하세요."])
+        if not re.match(REGEX_EMAIL,email):
+            return JsonResponse({"message":"VALIDATION_ERROR"}, status=400)
         if User.objects.filter(email=email).exists():
-            return JsonResponse({"message":"DUPLICATION_ERROR"}, status=400)
+            return JsonResponse({"message":"VALIDATION_ERROR"}, status=400)
 
-    def validate_password(password):
+    def validate_password(self, password):
         REGEX_PASSWORD = '^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,}$'
-        if not re.fullmatch(REGEX_PASSWORD, password):
-            return "비밀번호를 확인하세요. 최소 1개 이상의 소문자, 대문자, 숫자, 특수문자로 구성 되어야 하며 길이는 8자리 이상이어야 합니다."
-            
+        if not re.match(REGEX_PASSWORD, password):
+            return JsonResponse({"message":"VALIDATION_ERROR"}, status=400)
