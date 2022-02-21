d5560c614a7785a2323767e55c2ae6657e758841
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-02-21 17:10:34 +0900

    add:login1

--- students/geonwoo/users/views.py
+++ students/geonwoo/users/views.py
@@ -35,3 +35,20 @@ class UserView(View):
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"},status=400)
             
+class LoginView(View):
+    def post(self, request):
+        try:
+            data  = json.loads(request.body)
+           
+            if not User.objects.filter(email = data['email']).exists():
+                return JsonResponse({"message":"INVALID_USER"},status=401)
+            
+            if not User.objects.filter(password = data['password']).exists():
+                return JsonResponse({"message":"INVALID_PASSWORD"},status=401)
+          
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+        
+            
+        return JsonResponse({"message": "SUCCESS"}, status=200)
